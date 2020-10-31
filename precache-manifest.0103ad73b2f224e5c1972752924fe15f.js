7bbc691d2714b220bc29dfecce4277b9e1032a64
Nicolás <niparrara@unal.edu.co>
2020-10-31 13:03:23 -0500

    Updates

---
+++ precache-manifest.0103ad73b2f224e5c1972752924fe15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3b123ce18789559638b8607cf47252",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "dc27e8b8a25fe0611bb5",
+    "url": "/portafolio/static/css/main.e3dd21db.chunk.css"
+  },
+  {
+    "revision": "eb0825e318dcae316fb4",
+    "url": "/portafolio/static/js/2.f3b93f86.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portafolio/static/js/2.f3b93f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc27e8b8a25fe0611bb5",
+    "url": "/portafolio/static/js/main.3ff4a55d.chunk.js"
+  },
+  {
+    "revision": "9efaed5fcd6d509e83da",
+    "url": "/portafolio/static/js/runtime-main.85b9bf3b.js"
+  }
+]);
